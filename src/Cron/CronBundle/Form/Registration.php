4e76598a9ee68508bb5472f5585d8af3763ab28d
Fyodor Nezhivoy <gyzerok@gmail.com>
2012-12-21 17:02:29 +0600

    registration form modified

--- src/Cron/CronBundle/Form/Registration.php
+++ src/Cron/CronBundle/Form/Registration.php
@@ -14,20 +14,19 @@ class Registration extends AbstractType
             $days[$i] = $i;
         $months = array("Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь");
         $years = array();
-        for ($i = 1950; $i <= date('Y'); $i++)
+        for ($i = date('Y') - 62; $i <= date('Y'); $i++)
             $years[$i] = $i;
 
         $builder->add('email', 'email', array('label' => 'Email', 'required' => true))
-                ->add('userpass', 'password', array('label' => 'Пароль', 'required' => true))
-                //->add('userpassAgain', 'password', array('label' => 'Повторите пароль', 'required' => true))
+                ->add('userpass', 'repeated', array('required' => true, 'first_name' => 'Password', 'second_name' => 'Confirm', 'type' => 'password'))
                 ->add('username', null, array('label' => 'Ваше имя', 'required' => true))
                 ->add('gender', 'choice', array('label' => 'Пол', 'choices' => array(1 => 'Мужской', 2 => 'Женский'), 'expanded' => true, 'required' => true))
-                ->add('birthDate', 'birthday', array('label' => 'День', 'required' => true))//, 'choices' => $days, 'expanded' => true))
-                //->add('month', 'choice', array('label' => 'Месяц', 'choices' => $months, 'expanded' => true, 'required' => true))
-                //->add('year', 'choice', array('label' => 'Год', 'choices' => $years, 'expanded' => true, 'required' => true))
+                ->add('birthDate', 'birthday', array('label' => 'Дата рождения', 'format' => 'dd MM yyyy', 'years' => $years, 'required' => true))
                 ->add('country', null, array('label' => 'Страна', 'empty_value' => 'Выберите страну', 'required' => true))
                 ->add('state', null, array('label' => 'Регион', 'empty_value' => 'Выберите регион', 'disabled' => true, 'required' => true))
-                ->add('city', null, array('label' => 'Город', 'empty_value' => 'Выберите город', 'disabled' => true, 'required' => true));
+                ->add('city', null, array('label' => 'Город', 'empty_value' => 'Выберите город', 'disabled' => true, 'required' => true))
+                ->add('agreement', null, array('label' => 'Правила', 'required' => true));
+
     }
 
     public function getName()
