93a01d8e2d147bce003e62d0892b1ffbdb5e227e
Fyodor Nezhivoy <gyzerok@gmail.com>
2012-12-25 16:09:59 +0600

    some login changes

---
+++ src/Cron/CronBundle/Controller/SecutiryController.php
@@ -0,0 +1,27 @@
+<?php
+namespace Cron\CronBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
+
+class SecurityController extends Controller
+{
+    public function loginAction()
+    {
+        $request = $this->getRequest();
+        $session = $request->getSession();
+
+        // получить ошибки логина, если таковые имеются
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+        }
+
+        return $this->render('CronCronBundle:Security:index.html.twig', array(
+            // имя, введённое пользователем в последний раз
+            'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+            'error'         => $error,
+        ));
+    }
+}
